851cd010328dbbdd4bb00d67358e35cc6feed271
Mikilio <kilian.mio@tum.de>
2022-12-19 14:25:32 +0100

    cleanup

--- modules/nvim/lua/plugins/telescope.lua
+++ modules/nvim/lua/plugins/telescope.lua
@@ -1,119 +1,56 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-	return
+  return
 end
 
-telescope.load_extension("media_files")
-
-local actions = require("telescope.actions")
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fx', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fp', telescope.extensions.project.project, {})
+vim.keymap.set('n', '<leader>fw', telescope.extensions.git_worktree.git_worktrees, {})
+vim.keymap.set('n', '<leader>nw', telescope.extensions.git_worktree.create_git_worktree, {})
 
 telescope.setup({
-	defaults = {
+  defaults = {
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    color_devicons = true,
+    sorting_strategy = "ascending",
+    layout_config = {
+      prompt_position = "top",
+      horizontal = {
+        width_padding = 0.04,
+        height_padding = 0.1,
+        preview_width = 0.6,
+      },
+      vertical = {
+        width_padding = 0.05,
+        height_padding = 1,
+        preview_height = 0.5,
+      },
+    },
+  },
 
-		prompt_prefix = " ",
-		selection_caret = " ",
-		path_display = { "smart" },
-		color_devicons = true,
-		sorting_strategy = "ascending",
-		layout_config = {
-			prompt_position = "top",
-			horizontal = {
-				width_padding = 0.04,
-				height_padding = 0.1,
-				preview_width = 0.6,
-			},
-			vertical = {
-				width_padding = 0.05,
-				height_padding = 1,
-				preview_height = 0.5,
-			},
-		},
-
-		mappings = {
-			i = {
-				["<C-n>"] = actions.cycle_history_next,
-				["<C-p>"] = actions.cycle_history_prev,
-
-				["<C-j>"] = actions.move_selection_next,
-				["<C-k>"] = actions.move_selection_previous,
-
-				["<C-c>"] = actions.close,
-
-				["<Down>"] = actions.move_selection_next,
-				["<Up>"] = actions.move_selection_previous,
-
-				["<CR>"] = actions.select_default,
-				["<C-x>"] = actions.select_horizontal,
-				["<C-v>"] = actions.select_vertical,
-				["<C-t>"] = actions.select_tab,
-
-				["<C-u>"] = actions.preview_scrolling_up,
-				["<C-d>"] = actions.preview_scrolling_down,
-
-				["<PageUp>"] = actions.results_scrolling_up,
-				["<PageDown>"] = actions.results_scrolling_down,
-
-				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-				["<C-l>"] = actions.complete_tag,
-				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-			},
-
-			n = {
-				["<esc>"] = actions.close,
-				["<CR>"] = actions.select_default,
-				["<C-x>"] = actions.select_horizontal,
-				["<C-v>"] = actions.select_vertical,
-				["<C-t>"] = actions.select_tab,
-
-				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-				["j"] = actions.move_selection_next,
-				["k"] = actions.move_selection_previous,
-				["H"] = actions.move_to_top,
-				["M"] = actions.move_to_middle,
-				["L"] = actions.move_to_bottom,
-
-				["<Down>"] = actions.move_selection_next,
-				["<Up>"] = actions.move_selection_previous,
-				["gg"] = actions.move_to_top,
-				["G"] = actions.move_to_bottom,
-
-				["<C-u>"] = actions.preview_scrolling_up,
-				["<C-d>"] = actions.preview_scrolling_down,
-
-				["<PageUp>"] = actions.results_scrolling_up,
-				["<PageDown>"] = actions.results_scrolling_down,
-
-				["?"] = actions.which_key,
-			},
-		},
-	},
-	pickers = {
-		-- Default configuration for builtin pickers goes here:
-		-- picker_name = {
-		--   picker_config_key = value,
-		--   ...
-		-- }
-		-- Now the picker_config_key will be applied every time you call this
-		-- builtin picker
-	},
-	extensions = {
-		media_files = {
-			-- filetypes whitelist
-			-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-			filetypes = { "png", "webp", "jpg", "jpeg", "mp4", "pdf", "webm" },
-			find_cmd = "rg", -- find command (defaults to `fd`)
-		},
-		-- Your extension configuration goes here:
-		-- extension_name = {
-		--   extension_config_key = value,
-		-- }
-		-- please take a look at the readme of the extension you want to configure
-	},
+  extensions = {
+    media_files = {
+      filetypes = { "png", "webp", "jpg", "jpeg", "mp4", "pdf", "webm" },
+      find_cmd = "rg", -- find command (defaults to `fd`)
+    },
+    project = {
+      base_dirs = {
+        {'~/.', max_depth = 1},
+        {'~/Src'},
+      },
+    }
+  }
 })
+
+telescope.load_extension("media_files")
+telescope.load_extension("fzy_native")
+telescope.load_extension("project");
+telescope.load_extension("git_worktree");
--- modules/nvim/lua/settings.lua
+++ modules/nvim/lua/settings.lua
@@ -1,80 +1,78 @@
-local opt = vim.opt
 local g = vim.g
 local api = vim.api
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
 
 -- cursor behavior
 local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
 api.nvim_create_autocmd(
   { "InsertLeave", "WinEnter" },
-  { pattern = "*", command = "set cursorline", group = cursorGrp }
-)
+  {
+    pattern = "*",
+    command = "set cursorline",
+    group = cursorGrp
+  })
 api.nvim_create_autocmd(
-  { "InsertEnter", "WinLeave" },
-  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+	{ "InsertEnter", "WinLeave" },
+	{
+    pattern = "*",
+    command = "set nocursorline",
+    group = cursorGrp
+  }
 )
 
--- highlight on yank
-local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
-api.nvim_create_autocmd("TextYankPost", {
-  command = "silent! lua vim.highlight.on_yank()",
-  group = yankGrp,
+--remove trailing whitespace
+autocmd({ "BufWritePre" }, {
+	pattern = "*",
+	command = "%s/\\s\\+$//e",
 })
 
--- reduce shiftwidth in certain filtypes
-api.nvim_create_autocmd(
-  "FileType",
-  { pattern = { "nix", "lua" }, command = "setlocal shiftwidth=2" }
-)
+-- highlight on yank
+local yank_group = augroup("HighlightYank", {})
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
+
+local options  = {
+	mouse = "a",
+	undofile = true,
+	ignorecase = true,
+	smartcase = true,
+	showmode = false,
+	showtabline = 2,
+	swapfile = false,
+	hidden = true, --default on
+	expandtab = true,
+	cmdheight = 1,
+	shiftwidth = 2, --insert 2 spaces for each indentation
+	tabstop = 2, --insert 2 spaces for a tab
+	cursorline = true, --Highlight the line where the cursor is located
+	cursorcolumn = false,
+	number = true,
+	numberwidth = 4,
+	relativenumber = true,
+	wrap = false,
+	scrolloff = 8,
+	fileencodings = "utf-8,gbk",
+	updatetime = 50, -- faster completion (4000ms default)
+  incsearch = true,
+	foldenable = false,
+	foldlevel = 99,
+}
 
 -- Keybinds
-require('keymaps')
--- Performance
-opt.lazyredraw = true
-opt.shell = "bash"
-opt.shadafile = "NONE"
+require("keymaps")
 
--- Colors
-opt.termguicolors = true
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
 
--- Undo files
-opt.undofile = true
-
--- Indentation (autoindentation behavior on by default)
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.shiftround = true
-opt.expandtab = true
-opt.scrolloff = 15
-
--- Set clipboard to use system clipboard
-opt.clipboard = "unnamedplus"
-
--- Use mouse
-opt.mouse = "a"
-
--- Nicer UI settings
-opt.cursorline = true
-opt.relativenumber = true
-opt.number = true
-
--- Get rid of annoying viminfo file
-opt.viminfo = ""
-opt.viminfofile = "NONE"
-
--- Miscellaneous quality of life
-opt.ignorecase = true
-opt.ttimeoutlen = 5
-opt.hidden = true
-opt.shortmess = "atI"
-opt.wrap = false
-opt.backup = false
-opt.writebackup = false
-opt.errorbells = false
-opt.swapfile = false
-opt.showmode = false
-opt.laststatus = 3
-opt.pumheight = 6
-opt.splitright = true
-opt.splitbelow = true
-opt.completeopt = "menuone,noselect"
-
+g.netrw_banner = 0
