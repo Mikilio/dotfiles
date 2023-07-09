e5c311953e0e63c06fd9098cb6cff9737e1f8a6c
Mikilio <official.mikilio+dev@gmail.com>
2023-07-09 08:56:24 +0200

    added prper git and gpg support

--- home/applications/gpg.nix
+++ home/applications/gpg.nix
@@ -17,6 +17,7 @@
       pinentryFlavor = "gnome3";
       sshKeys = [
         "962C29E85C5026E104466143BA6CE4D7F95B81A9"
+        "947AD9A76E3CC264849444CC4A2A650F9E76B5FF"
       ];
     };
   };
--- home/shells/git.nix
+++ home/shells/git.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  config,
   ...
 } : {
   config.programs.git = {
@@ -9,7 +10,80 @@
       delta.enable = true;
       signing = {
         signByDefault = true;
-        key = "962C29E85C5026E104466143BA6CE4D7F95B81A9";
+        key = "5B2F1A890CF33F3F!";
+      };
+      attributes = [
+        "*.pdf diff=pdf"
+      ];
+      lfs = {
+        enable = true;
+        skipSmudge = true;
+      };
+      extraConfig = {
+        interactive.singleKey = true;
+        format.signoff = true;
+        color = {
+          branch = true;
+          diff = true;
+          status = true;
+        };
+        push.recurseSubmodules = "on-demand";
+        am.threeWay = true;
+        diff = {
+          renames = true;
+          colorMoved = true;
+        };
+        merge = {
+          tool = config.home.shells.editor;
+          conflictStyle = "zdiff3";
+        };
+        push = {
+          default = "simple";
+          autoSetupRemote = true;
+        };
+        rerere = {
+          autoUpdate = true;
+          enabled = true;
+        };
+        branch.autoSetupRebase = "always";
+        pull.rebase = true;
+        rebase = {
+          stat = true;
+          autoStash = true;
+          autoSquash = true;
+          updateRefs = true;
+        };
+        mergetool.prompt = false;
+        sendemail = {
+          smtpServer = "smtp.gmail.com";
+          smtpServerPort = 587;
+          smtpEncryption = "tls";
+          smtpUser = "official.mikilio@gmail.com";
+        };
+        credential.helper = "libsecret";
+        advice = {
+          detachedHead = false;
+          skippedCherryPicks = false;
+        };
+        init.defaultBranch = "main";
+        commit.verbose = true;
+      };
+      aliases = {
+        rb = "rebase origin/HEAD";
+        get = "clone --recursive";
+        blame = "-w -M";
+        update = "!git pull && git submodule update --init --recursive";
+        comma = "commit --amend";
+        uncommit = "reset --soft HEAD^";
+        pr = ''
+          !"pr() { git fetch origin pull/$1/head:pr-$1; git checkout pr-$1; }; pr"
+        '';
+        backport = "cherry-pick -x";
+        reset-pr = "reset --hard FETCH_HEAD";
+        force-push = "push --force-with-lease";
+        publish = "!git pull && git push";
+        # recover failed commit messages: https://stackoverflow.com/questions/9133526/git-recover-failed-commits-message
+        recommit = "!git commit -eF $(git rev-parse --git-dir)/COMMIT_EDITMSG";
       };
   };
 }
