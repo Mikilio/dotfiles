23ab48446e0077015f8f57e8a5b711d54127de4a
Mikilio <official.mikilio@gmail.com>
2025-11-23 19:20:06 +0100

    fix: better quality to remove crackling sound

--- nixos/modules/pipewire.nix
+++ nixos/modules/pipewire.nix
@@ -1,14 +1,13 @@
 {
-  lib,
+  pkgs,
   config,
   ...
-}:
-{
+}: {
   boot = {
     extraModulePackages = with config.boot.kernelPackages; [
       v4l2loopback.out
     ];
-    kernelModules = [ "v4l2loopback" ];
+    kernelModules = ["v4l2loopback"];
     extraModprobeConfig = ''
       options v4l2loopback exclusive_caps=1
     '';
@@ -23,18 +22,47 @@
     wireplumber = {
       enable = true;
 
-      extraConfig.bluetoothEnhancements = {
-        "monitor.bluez.properties" = {
-          "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = true;
-          "bluez5.enable-hw-volume" = [ "a2dp_sink" ];
-          "bluez5.auto-connect" = [ "a2dp_sink" ];
-          "bluez5.codecs" = [
-            "sbc" "sbc_xq"
-            "aac" "ldac"
-          ];
-        };
-      };
+      configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/50-hdmi-stereo-quality.conf" ''
+          context.properties = {
+            resample.quality = 10
+          }
+
+          monitor.alsa.rules = [
+            {
+              matches = [
+                {
+                  api.alsa.path = "hdmi-stereo"
+                }
+              ]
+              actions = {
+                update-props = {
+                  api.alsa.period-size = 2048
+                }
+              }
+            }
+          ]
+        '')
+
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/51-wh-1000xm3-ldac-hq.conf" ''
+          monitor.bluez.rules = [
+            {
+              matches = [
+                {
+                  device.name = "~bluez_card.*"
+                  device.product.id = "0x0cd3"
+                  device.vendor.id = "usb:054c"
+                }
+              ]
+              actions = {
+                update-props = {
+                  bluez5.a2dp.ldac.quality = "hq"
+                }
+              }
+            }
+          ]
+        '')
+      ];
     };
   };
   security.rtkit.enable = true;
