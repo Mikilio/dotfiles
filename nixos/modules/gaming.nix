6ee8f4c831ae86f2ff114515a25e2d0c90767a6d
Mikilio <official.mikilio@gmail.com>
2026-05-20 08:29:37 +0200

    fix: persist gamer

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -1,60 +1,71 @@
 {
   inputs,
-  config,
+  options,
   lib,
   pkgs,
   ...
 }: {
-  programs = {
-    steam = {
-      enable = true;
-      gamescopeSession = {
-        enable = true;
-        args = [
-          "--prefer-vk-device"
-          "1002:73df"
-          "--backend"
-          "drm"
-          "--prefer-output"
-          "DP-8,DP-9,eDP-1"
-        ];
-        env = {
-          MESA_VK_DEVICE_SELECT = "1002:73df";
-        };
-      };
-      extraCompatPackages = [
-        pkgs.proton-ge-bin
-      ];
-
-      # fix gamescope inside steam
-      package = pkgs.steam;
-    };
-    gamescope.capSysNice = true;
-    gamemode = {
-      enable = true;
-      settings = {
-        general = {
-          softrealtime = "auto";
-          renice = 15;
-        };
-      };
-    };
-  };
-
-  services = {
-    # see https://github.com/fufexan/nix-gaming/#pipewire-low-latency
-    pipewire.lowLatency = {
-      enable = true;
-      quantum = 32;
-      alsa.enable = true;
-    };
-  };
-
-  hardware = {
-    steam-hardware.enable = true;
-  };
-  boot.extraModprobeConfig = ''options bluetooth disable_ertm=1 '';
   imports = [
     inputs.nix-gaming.nixosModules.pipewireLowLatency
   ];
+  config =
+    {
+      programs = {
+        steam = {
+          enable = true;
+          gamescopeSession = {
+            enable = true;
+            args = [
+              "--prefer-vk-device"
+              "1002:73df"
+              "--backend"
+              "drm"
+              "--prefer-output"
+              "DP-8,DP-9,eDP-1"
+            ];
+            env = {
+              MESA_VK_DEVICE_SELECT = "1002:73df";
+            };
+          };
+          extraCompatPackages = [
+            pkgs.proton-ge-bin
+          ];
+
+          # fix gamescope inside steam
+          package = pkgs.steam;
+        };
+        gamescope.capSysNice = true;
+        gamemode = {
+          enable = true;
+          settings = {
+            general = {
+              softrealtime = "auto";
+              renice = 15;
+            };
+          };
+        };
+      };
+
+      services = {
+        # see https://github.com/fufexan/nix-gaming/#pipewire-low-latency
+        pipewire.lowLatency = {
+          enable = true;
+          quantum = 32;
+          alsa.enable = true;
+        };
+      };
+
+      hardware = {
+        steam-hardware.enable = true;
+      };
+      boot.extraModprobeConfig = ''options bluetooth disable_ertm=1 '';
+    }
+    // lib.optionalAttrs (options.environment?persistence)
+    {
+      environment.persistence = {
+        "/persistent/cache".directories = [
+          "/home/gamer"
+        ];
+      };
+    };
 }
